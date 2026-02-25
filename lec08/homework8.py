81e8ea5a75b4d73b78e3956a68d9153e19d6dc7f
ouketsukei-source <ou.ketsu.kei@gmail.com>
2026-02-25 19:43:45 +0900

    Update homework8.py

--- lec08/homework8.py
+++ lec08/homework8.py
@@ -1,45 +1,28 @@
 import numpy as np
 
 def waveform_to_frames(waveform, frame_length, step):
-    '''
-    Chop a waveform into overlapping frames.
-    
-    @params:
-    waveform (np.ndarray(N)) - the waveform
-    frame_length (scalar) - length of the frame, in samples
-    step (scalar) - step size, in samples
-    
-    @returns:
-    frames (np.ndarray((num_frames, frame_length))) - waveform chopped into frames
-       frames[m/step,n] = waveform[m+n] only for m = integer multiple of step
-    '''
-    raise RuntimeError("You need to change this part")
+    N = len(waveform)
+    num_frames = 1 + (N - frame_length) // step
+
+    frames = np.zeros((num_frames, frame_length))
+
+    for m in range(num_frames):
+        start = m * step
+        end = start + frame_length
+        frames[m, :] = waveform[start:end]
+
+    return frames
 
 def frames_to_mstft(frames):
-    '''
-    Take the magnitude FFT of every row of the frames matrix.
-    
-    @params:
-    frames (np.ndarray((num_frames, frame_length))) - the speech samples
-    
-    @returns:
-    mstft (np.ndarray((num_frames, frame_length))) - the magnitude short-time Fourier transform
-    '''
-    raise RuntimeError("You need to change this part")
+    return np.abs(np.fft.fft(frames, axis=1))
 
 def mstft_to_spectrogram(mstft):
-    '''
-    Convert max(0.001*amax(mstft), mstft) to decibels.
-    
-    @params:
-    stft (np.ndarray((num_frames, frame_length))) - magnitude short-time Fourier transform
-    
-    @returns:
-    spectrogram (np.ndarray((num_frames, frame_length)) - spectrogram 
-    
-    The spectrogram should be expressed in decibels (20*log10(mstft)).
-    np.amin(spectrogram) should be no smaller than np.amax(spectrogram)-60
-    '''
-    raise RuntimeError("You need to change this part")
+    eps = 0.001 * np.max(mstft)
+    mstft = np.maximum(eps, mstft)
 
+    spectrogram = 20 * np.log10(mstft)
 
+    floor = np.max(spectrogram) - 60
+    spectrogram = np.maximum(spectrogram, floor)
+
+    return spectrogram
