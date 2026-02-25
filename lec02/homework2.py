fd61bb1409b3fb6af127c32ca505daacbe31be44
ouketsukei-source <ou.ketsu.kei@gmail.com>
2026-02-25 19:41:59 +0900

    Update homework2.py

--- lec02/homework2.py
+++ lec02/homework2.py
@@ -1,16 +1,7 @@
-'''
-This homework defines one method, called "arithmetic".
-that method, type `help homework2.arithmetic`.
-'''
+import numpy as np
 
-def arithmetic(x, y):
-    """
-    Modify this code so that it performs one of four possible functions, 
-    as specified in the following table:
+def dot_product(x, y):
+    return np.dot(x, y)
 
-                        isinstance(x,str)  isinstance(x,float)
-    isinstance(y,str)   return x+y         return str(x)+y
-    isinstance(y,float) return x*int(y)    return x*y
-    """
-    return 0
-
+def matrix_multiply(A, B):
+    return np.dot(A, B)
