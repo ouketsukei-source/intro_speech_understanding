b97661553a5860cf8d46b364537c6c99faa3190d
ouketsukei-source <ou.ketsu.kei@gmail.com>
2026-02-25 19:42:35 +0900

    Update homework4.py

--- lec04/homework4.py
+++ lec04/homework4.py
@@ -1,18 +1,7 @@
+import numpy as np
 
-def next_birthday(date, birthdays):
-    '''
-    Find the next birthday after the given date.
-
-    @param:
-    date - a tuple of two integers specifying (month, day)
-    birthdays - a dict mapping from date tuples to lists of names, for example,
-      birthdays[(1,10)] = list of all people with birthdays on January 10.
-
-    @return:
-    birthday - the next day, after given date, on which somebody has a birthday
-    list_of_names - list of all people with birthdays on that date
-    '''
-    birthday = (1,1)
-    list_of_names = []
-    return birthday, list_of_names
-    
+def fourier_matrix(N):
+    k = np.arange(N).reshape((N, 1))
+    n = np.arange(N).reshape((1, N))
+    W = np.exp(-2j * np.pi * k * n / N)
+    return W
