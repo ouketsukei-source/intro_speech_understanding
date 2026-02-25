23c2b481eaad2c2bae8a5a17d4b50caa39d150a1
ouketsukei-source <ou.ketsu.kei@gmail.com>
2026-02-25 19:42:21 +0900

    Update homework3.py

--- lec03/homework3.py
+++ lec03/homework3.py
@@ -1,18 +1,4 @@
+import numpy as np
 
-def words2characters(words):
-    """
-    This function converts a list of words into a list of characters.
-
-    @param:
-    words - a list of words
-
-    @return:
-    characters - a list of characters
-
-    Every element of "words" should be converted to a str, then split into
-    characters, each of which is separately appended to "characters." For 
-    example, if words==['hello', 1.234, True], then characters should be
-    ['h', 'e', 'l', 'l', 'o', '1', '.', '2', '3', '4', 'T', 'r', 'u', 'e']
-    """
-    return []
-
+def convolve(x, h):
+    return np.convolve(x, h)
